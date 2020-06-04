9da3598b9b79a509d0474df93723f042be071292
Han Myat Thu <hanmyatthu111@gmail.com>
2020-06-04 12:24:52 +0630

    db seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -6,7 +6,9 @@ use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $work = $faker->word;
     return [
-        //
+        'name' => $work,
+        'slug' => str_slug($work)
     ];
 });
