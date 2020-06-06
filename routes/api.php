515b0866c5dc3f35f954cf8074deed59c989fbbe
Han Myat Thu <hanmyatthu111@gmail.com>
2020-06-06 14:32:32 +0630

    like & dislikes

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ Route::apiResource('/questions', 'QuestionController');
 Route::apiResource('/categories', 'CategoryController');
 
 Route::apiResource('/questions/{question}/replies', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@likeIt');
+
+Route::delete('/like/{reply}','LikeController@unLikeIt');
