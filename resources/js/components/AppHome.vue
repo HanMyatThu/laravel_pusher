c7f8406d322e6c47164ac34e813d315b584a34ff
Han Myat Thu <hanmyatthu111@gmail.com>
2020-06-18 00:06:28 +0630

    login component % login

--- resources/js/components/AppHome.vue
+++ resources/js/components/AppHome.vue
@@ -1,15 +1,18 @@
 <template>
    <div>
         <toolbar></toolbar>
+        <router-view></router-view>
         <AppFooter></AppFooter>
    </div>
 </template>
 
 <script>
-import toolbar from './Toolbar'
-import AppFooter from './AppFooter'
+import toolbar from './Toolbar';
+import AppFooter from './AppFooter';
+import login from './login/Login';
+
     export default {
-        components:{toolbar,AppFooter}
+        components:{toolbar,AppFooter,login}
     }
 </script>
 
--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -6,29 +6,30 @@
     tile
   >
     <v-toolbar dense>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+      <!-- <v-app-bar-nav></v-app-bar-nav-icon> -->
 
       <v-toolbar-title>Draz</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn flat>
-          Forum
-      </v-btn>
-
-      <v-btn flat>
-          Ask Question
-      </v-btn>
-
-      <v-btn flat>
-          Category
-      </v-btn>
-
-      <v-btn flat>
-          Login
-      </v-btn>
-
-
+      <div class="hidden-sm-and-down">
+          <router-link to='/forum'>
+              <v-btn>
+                  Forum
+              </v-btn>
+          </router-link>
+            <v-btn>
+                  Ask Question
+            </v-btn>
+            <v-btn>
+                Category
+            </v-btn>
+          <router-link to='/login'>
+              <v-btn>
+                  Login
+              </v-btn>
+          </router-link>
+      </div>
     </v-toolbar>
   </v-card>
 </template>
